67f830a3ed2a7c362a4895fb6bb6c763cb5159d9
HamanJosh <109014545+HamanJosh@users.noreply.github.com>
2022-07-10 14:59:39 +0900

    Create main.c

---
+++ chapter1_make/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("==================================\n");
+    printf("=========== Chapter. 1 ===========\n");
+    printf("==================================\n");
+    printf("Hello World!\n");
+    printf("\n");
+
+    return 0;
+}
